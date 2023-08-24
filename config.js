c064d459f4c3ae2fe1b26e4efa3f993e1e48819e
WR <adm@hamont.com.br>
2023-08-24 16:14:06 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPhTabB7gNHdutY6CH7F9CoC0lxQS7IHw",
+    authDomain: "upload-de-imagens-396117.firebaseapp.com",
+    projectId: "upload-de-imagens-396117",
+    storageBucket: "upload-de-imagens-396117.appspot.com",
+    messagingSenderId: "875602451810",
+    appId: "1:875602451810:web:acbaab244dfa8bfecc603b"
+  };
